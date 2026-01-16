ff71f353e23f5db0aca989fbc0baefadbf02586d
Alex Fazio <alessandro.fazio@me.com>
2026-01-16 14:45:55 +0100

    Auto-sync: .config/nvim/lazy-lock.json,.config/nvim/lua/config/options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Python provider - dedicated venv for Neovim plugins (pynvim)
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/venv/bin/python")
+
 -- Font configuration for GUI clients (Neovide, VimR, etc.)
 vim.opt.guifont = "IosevkaTerm Nerd Font:h14"
